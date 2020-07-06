52bafc4b03029f4f5752c3c4a69ad8aaf717a8f0
Mior Muhammad Zaki <crynobone@gmail.com>
2020-07-06 10:56:10 +0800

    Add ubb check.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/One/Vehicle.php
+++ src/One/Vehicle.php
@@ -15,6 +15,30 @@ class Vehicle extends Request
      */
     public function submit(array $payload): Response
     {
+        return $this->information($payload);
+    }
+
+    /**
+     * Send vehicle information request.
+     *
+     * @param array $payload
+     *
+     * @return \Allianz\MotorInsurance\Response
+     */
+    public function information(array $payload): Response
+    {
         return $this->sendJson('POST', 'vehicleDetails', $this->getApiHeaders(), $this->mergeApiBody($payload));
+    }
+
+    /**
+     * Send vehicle verification request.
+     *
+     * @param array $payload
+     *
+     * @return \Allianz\MotorInsurance\Response
+     */
+    public function verification(array $payload): Response
+    {
+        return $this->sendJson('POST', 'checkUBB', $this->getApiHeaders(), $this->mergeApiBody($payload));
     }
 }
